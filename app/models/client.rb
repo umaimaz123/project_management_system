d0dcc153a47ad852f0fddbd4dd6be7890a72a67d
umaimaz123 <umaima749zulfiqar@gmail.com>
2022-01-03 17:51:18 +0500

    improved code quality

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,13 +1,9 @@
 class Client < ApplicationRecord
- validates :first_name, :last_name, :email, :website, presence: true 
- validates :website, format: { with: URI.regexp,
-                                message: 'website is invalid follow the pattern of http://example@gmail.com' }
- validates :email, uniqueness: true,
-                   format: { with: /\A[^@\s]+@[^@\s]+\z/,
-                              message: 'email is invalid follow the pattern of example@gmail.com' }
- validates :phone, numericality: true,
-                  length: { maximum: 12 }
+  validates :first_name, :last_name, :email, :website, presence: true 
+  validates :website, format: { with: URI.regexp, message: 'website is invalid follow the pattern of http://example@gmail.com' }
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'email is invalid follow the pattern of example@gmail.com' }
+  validates :phone, numericality: true, length: { maximum: 12 }
 
- has_one_attached :image
- belongs_to :user
+  has_one_attached :image
+  belongs_to :user
 end
